de2214eadd2389b94aa96287287b5945e6698b51
DanielElisenberg <daniel@elisenberg.com>
2020-12-06 17:00:51 +0100

    Day03: split function, better naming

--- day03/day03.py
+++ day03/day03.py
@@ -3,10 +3,14 @@ from copy import deepcopy
 import numpy as np
 
 
-def examine_slope(map, angle):
-    map = deepcopy(map)
+def align_slope(map, angle):
     for y in range(len(map)):
         map[y].rotate(y*-angle)
+
+
+def trees_encountered(map, angle):
+    map = deepcopy(map)
+    align_slope(map, angle)
     return len(
         [location[0] for location in map if location[0] == '#']
     )
@@ -15,11 +19,10 @@ def examine_slope(map, angle):
 with open('day03/input.data') as input:
     map = [deque(line) for line in input.read().splitlines()]
 
-trees_encountered = examine_slope(map, 3)
-print(f'first solution: {trees_encountered}')
+print(f'first solution: {trees_encountered(map, 3)}')
 
-trees_encountered = np.product(
-    [examine_slope(m, a) for (m, a) in [
+total_trees_encountered = np.product(
+    [trees_encountered(m, a) for (m, a) in [
      (map, 1), (map, 3), (map, 5), (map, 7), (map[::2], 1)]]
 )
-print(f'second solution: {trees_encountered}')
+print(f'second solution: {total_trees_encountered}')
