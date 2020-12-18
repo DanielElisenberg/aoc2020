46847587ad28e282ed79bab28022aec3b0b85f2d
DanielElisenberg <daniel@elisenberg.com>
2020-12-18 21:12:13 +0100

    Day18: done

--- day18/day18.py
+++ day18/day18.py
@@ -14,7 +14,7 @@ class Node:
             return self.left.get_value() * self.right.get_value()
 
 
-def value_or_nested_expression(line, i):
+def value_or_nested_expression(line, i, parse_expression):
     if line[i] == '(':
         open_parenthesis = 1
         for j in range(i+1, len(line)):
@@ -29,23 +29,51 @@ def value_or_nested_expression(line, i):
         return (Node(line[i]), i+1)
 
 
-def parse_expression(line):
-    (root, i) = value_or_nested_expression(line, 0)
+def parse_expression_plus_precedence(line):
+    (root, i) = value_or_nested_expression(
+        line, 0, parse_expression_plus_precedence
+    )
     while i < len(line)-1:
         if line[i] == '+' and root.right is not None:
             new_branch = Node(line[i])
             new_branch.left = root.right
-            (new_branch.right, i) = value_or_nested_expression(line, i+1)
+            (new_branch.right, i) = value_or_nested_expression(
+                line, i+1, parse_expression_plus_precedence
+            )
             root.right = new_branch
         else:
             new_root = Node(line[i])
             new_root.left = root
-            (new_root.right, i) = value_or_nested_expression(line, i+1)
+            (new_root.right, i) = value_or_nested_expression(
+                line, i+1, parse_expression_plus_precedence
+            )
             root = new_root
+    return root
+
+
+def parse_expression_same_precedence(line):
+    (root, i) = value_or_nested_expression(
+        line, 0, parse_expression_same_precedence
+    )
+    while i < len(line)-1:
+        new_root = Node(line[i])
+        new_root.left = root
+        (new_root.right, i) = value_or_nested_expression(
+            line, i+1, parse_expression_same_precedence
+        )
+        root = new_root
     return root
 
 
 with open('day18/input.data') as input:
     lines = [line.replace(' ', '') for line in input.read().split('\n')]
 
-print(sum([parse_expression(line).get_value() for line in lines]))
+sum_of_homework = sum(
+    [parse_expression_same_precedence(line).get_value() for line in lines]
+)
+print(f'first solution: {sum_of_homework}')
+
+sum_of_homework = sum(
+    [parse_expression_plus_precedence(line).get_value() for line in lines]
+)
+print(f'second solution: {sum_of_homework}')
