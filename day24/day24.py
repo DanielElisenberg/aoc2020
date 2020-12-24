98225ad19a8f1d8c976e219a317b57014f71801f
DanielElisenberg <daniel@elisenberg.com>
2020-12-24 12:49:55 +0100

    day24: done

--- day24/day24.py
+++ day24/day24.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 walk = {
     'nw': (-0.5, 1),
     'ne': (0.5, 1),
@@ -6,6 +9,27 @@ walk = {
     'sw': (-0.5, -1),
     'se': (0.5, -1)
 }
+
+
+def flip_accordingly(tile_map):
+    black_adjecent = defaultdict(int)
+    for (x, y) in tile_map.keys():
+        black_adjecent[(x, y)] += 0
+        if tile_map[(x, y)] == 'black':
+            for (walk_x, walk_y) in walk.values():
+                black_adjecent[(x+walk_x, y+walk_y)] += 1
+
+    for coords, count in black_adjecent.items():
+        if count == 2:
+            if coords in tile_map.keys():
+                if tile_map[coords] == 'white':
+                    tile_map[coords] = 'black'
+            else:
+                tile_map[coords] = 'black'
+        elif count == 0 or count > 2:
+            if coords in tile_map.keys():
+                if tile_map[coords] == 'black':
+                    tile_map[coords] = 'white'
 
 
 with open('day24/input.data') as input:
@@ -31,3 +55,8 @@ for line in lines:
         tile_map[(x, y)] = 'black'
 black_tiles_count = len([1 for val in tile_map.values() if val == 'black'])
 print(f'first solution: {black_tiles_count}')
+
+for i in range(100):
+    flip_accordingly(tile_map)
+black_tiles_count = len([1 for val in tile_map.values() if val == 'black'])
+print(f'second solution: {black_tiles_count}')
